01608aa55733f70b9b557b52b101e9676dfdec6e
John Capovani <john@capovani.com>
2022-10-24 19:22:29 -0400

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -18,13 +18,15 @@ function Show({ bread, index }) {
             </p>
             <img src={bread.image} alt={bread.name} />
 
-
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
 
             <li><a href="/breads">Go home</a></li>
+
+
         </Default>
 
     )
